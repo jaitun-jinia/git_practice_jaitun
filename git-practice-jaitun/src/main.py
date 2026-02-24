1e84daec52679f1d9d16af2cf1ce86063a74c945
jaitun-jinia <nessajebun4@gmail.com>
2026-02-24 16:17:58 +0600

    Add basic calculator functions

--- git-practice-jaitun/src/main.py
+++ git-practice-jaitun/src/main.py
@@ -1,4 +1,8 @@
 import datetime
+from utils import add, subtract
 
 print("Name: Jaitun Jinia")
 print("Today's Date:", datetime.date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
